c6e90d5088f573551527d0c0cbd1a3ea6d01037e
keiko-y <crystal0829kei@gmail.com>
2021-03-08 10:55:53 +0900

    ユーザーモデル単体テスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_paramaters, if: :devise_controller?
+
+  protected
+  def configure_permitted_paramaters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday]
+    )
+  end
 end
