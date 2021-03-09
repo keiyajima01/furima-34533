2a6b82efaebcb10f83fdef169d152135f1c5a3f0
keiko-y <crystal0829kei@gmail.com>
2021-03-09 16:02:22 +0900

    コンフリクトの解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+  end
+
+  private
+
+  def item_params
+    @item = require(:item).permit(:name ,:image, :price, :category_id, :item_status, :shipping_cost_id, :shipping_area_id, :shipping_days_id)
   end
 end
