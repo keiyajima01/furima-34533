de7b4ce15204c0dd511e8dd9f7df5d3362922d9e
keiko-y <crystal0829kei@gmail.com>
2021-03-11 11:47:11 +0900

    JavaScript表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :text
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
     validates :price, format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width number." }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
     with_options numericality: { other_than: 0 } do
       validates :category_id
