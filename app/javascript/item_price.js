0fc315d66d8170e2a0c73594c35210a84fba7560
keiko-y <crystal0829kei@gmail.com>
2021-03-11 11:47:11 +0900

    JavaScript表示機能の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,13 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  console.log(priceInput);
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+      if (!isNaN(inputValue)) {
+        const inputValue = priceInput.value;
+        const addTaxDom = document.getElementById("add-tax-price");
+        addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+        const priceContentDom = document.getElementById("profit");
+        priceContentDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML)
+      };
+  });
 });
